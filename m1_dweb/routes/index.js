23073e1478f398200fdca675fd64cafd815794cd
vitorsrf <80456146+vitorsrf@users.noreply.github.com>
2023-05-14 14:38:46 +0100

    Delete m1_dweb directory

--- m1_dweb/routes/index.js
+++
@@ -1,6 +0,0 @@
-exports.users = require("./usersController");
-exports.secured = require("./securedController");
-exports.faturas = require("./faturasController");
-exports.clientes = require("./clientesController");
-exports.detalheFaturas = require("./detalheFaturasController");
-exports.produtos = require("./produtosController");
